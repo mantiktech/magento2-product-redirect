a26de9056e7ca86960146728ade1df4a20bf3271
Jor Ven <jorven@MacBook-Pro-de-Jor-2.local>
2023-06-24 20:09:12 -0300

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+Mantik Product Redirect extension for Magento 2.X
+
---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Mantik_ProductRedirect',
+    __DIR__
+);
